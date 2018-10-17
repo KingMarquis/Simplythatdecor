863d3a3c9a2ea2793e32549919266c386047056b
KingMarquis <pmclark04@gmail.com>
2018-10-17 00:30:08 -0500

    eslintrc added

---
+++ Components/Content.js
@@ -0,0 +1,13 @@
+import * as Pages from './Pages';
+
+export default function Content(state){
+    const page = state[state.active];
+
+    return `
+      <div id="content">
+        <div class="container">
+          ${Pages[page.body](state)}
+        </div>
+      </div>
+    `;
+}
---
+++ Components/Navigation.js
@@ -0,0 +1,28 @@
+function Link(link){
+    var href = '';
+
+    if(link !== 'home'){
+        href = link;
+    }
+
+    return `
+      <li>
+        <a href="/${href}" data-navigo>${link}</a>
+      </li>
+    `;
+}
+
+export default function Navigation(state){
+    var links = state
+        .links
+        .map(Link)
+        .join('');
+
+    return `
+      <div id="navigation">
+        <ul class="container">
+          ${links}
+        </ul>
+      </div>
+    `;
+}
